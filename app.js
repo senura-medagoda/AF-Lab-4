8f94b3e355185389d6bbe657f9a0a08ce01d5601
senura-medagoda <medagoda.senura@gmail.com>
2026-02-25 12:31:23 +0530

    Async await

--- app.js
+++ app.js
@@ -45,3 +45,14 @@ myPromise.then((result) => {
 }).catch((error) => {
  console.log(error);
 });
+
+async function myFunction() {
+ try {
+ const result = await myPromise;
+ console.log(result);
+ } catch (error) {
+ console.log(error);
+ }
+}
+
+myFunction();
