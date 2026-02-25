686b9d6882931421387eb2ccb2c01711d95005de
senura-medagoda <medagoda.senura@gmail.com>
2026-02-25 12:15:26 +0530

    Use a module

--- app.js
+++ app.js
@@ -29,3 +29,6 @@ https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
 }).on('error', (err) => {
  console.log("Error: " + err.message);
 })
+
+const myModule = require('./my-module.js');
+console.log(myModule.myFunction());
