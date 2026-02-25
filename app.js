4ae51ba9d6eecc13a8e0cbcdec9a2b6e91aad2e3
senura-medagoda <medagoda.senura@gmail.com>
2026-02-25 12:21:12 +0530

    Promises

--- app.js
+++ app.js
@@ -32,3 +32,16 @@ https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
 
 const myModule = require('./my-module.js');
 console.log(myModule.myFunction());
+
+const myPromise = new Promise((resolve, reject) => {
+ if (condition) {
+ resolve('Success!');
+ } else {
+ reject('Failure!');
+ }
+});
+myPromise.then((result) => {
+ console.log(result);
+}).catch((error) => {
+ console.log(error);
+});
